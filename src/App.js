086899b33d6d5fb529a94b511381b95b53177cf3
sheshruti <82359338+sheshruti@users.noreply.github.com>
2023-03-26 07:30:41 +0530

    Add files via upload
    updated src folder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import logo from './logo.svg';
+import React , { Component } from 'react';
+import HomePage from './components/HomePage';
+import Profile from './components/Profile';
+
+import { Route, Switch} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Navbar from './modules/Navbar.js'
-import Blog from './modules/Form.js'
-import About from './modules/About.js'
-import Footer from './modules/Footer.js'
-function App() {
-  return (
-    <div>
-      <Navbar/>
-      <Blog/>
-      <About/>
-      <Footer/>
-      <div className="bg-[#43bfc7] h-[auto] w-[screen] padding-[10px]">hello</div>
-    </div>
-  );
+import { Provider } from 'react-redux';
+import store from './store';
+import Navbar from './components/Navbar.js'
+
+class App extends Component {
+
+  render () {
+    return (
+      <>
+      
+      <Provider store={store}>
+            <Switch>
+              <Route exact path ="/profile" component={Profile}/>
+              
+            </Switch>
+        < HomePage/>
+      </Provider>
+      
+      </>
+    );
+}
 }
 
 export default App;
